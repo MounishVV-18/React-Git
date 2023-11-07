1bad09701de7b5f3aaddd5ab66769616ec336d37
MounishVV-18 <125252509+MounishVV-18@users.noreply.github.com>
2023-11-07 16:12:46 +0530

    Add files via upload

---
+++ DAY 2/ACTIVITY/src/App.js
@@ -0,0 +1,15 @@
+import './Assets/css/style.css';
+export const App =()=>{
+  return(
+<>
+<form className="c1">
+  <input type="text" name="" id="" placeholder="Name"className="fx1"/>
+  <input type='email' name='' id='' placeholder='Email'className='fx1'/>
+  <input type='number' name='' id='' placeholder='Phone'className='fx1'/>
+  <input type='text' name='' id='' placeholder='Username'className='fx1'/>
+  <input type='password' name='' id='' placeholder='Password'className='fx1'/>
+  <input type='submit' value='Register' className='fx2'/>
+  </form>          
+    </>
+  )
+}
